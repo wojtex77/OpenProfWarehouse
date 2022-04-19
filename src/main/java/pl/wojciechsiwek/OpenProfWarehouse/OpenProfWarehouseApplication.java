f31d4933c2e94e1b08da104d6caad39f240bd082
wojtex77 <37085911+wojtex77@users.noreply.github.com>
2022-04-19 13:00:00 +0200

    initializing commit

---
+++ src/main/java/pl/wojciechsiwek/OpenProfWarehouse/OpenProfWarehouseApplication.java
@@ -0,0 +1,13 @@
+package pl.wojciechsiwek.OpenProfWarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenProfWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenProfWarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wojciechsiwek/OpenProfWarehouse/OpenProfWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wojciechsiwek.OpenProfWarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenProfWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
