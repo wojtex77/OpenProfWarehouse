c43129c98234321523ce6c8c6ff757eb4d2108b7
wojtex77 <wojciech.siwek.programista@gmail.com>
2022-05-09 14:47:30 +0200

    restricting removing contrahent while in use

--- src/main/java/pl/wojciechsiwek/OpenProfWarehouse/contrahent/ContrahentService.java
+++ src/main/java/pl/wojciechsiwek/OpenProfWarehouse/contrahent/ContrahentService.java
@@ -1,16 +1,19 @@
 package pl.wojciechsiwek.OpenProfWarehouse.contrahent;
 
 import org.springframework.stereotype.Service;
+import pl.wojciechsiwek.OpenProfWarehouse.orders.OrderRepository;
 import pl.wojciechsiwek.OpenProfWarehouse.parts.PartRepository;
 
 @Service
 public class ContrahentService {
     private ContrahentRepository contrahentRepository;
     private PartRepository partRepository;
+    private OrderRepository orderRepository;
 
-    public ContrahentService(ContrahentRepository repository, PartRepository partRepository) {
+    public ContrahentService(ContrahentRepository repository, PartRepository partRepository, OrderRepository orderRepository) {
         this.contrahentRepository = repository;
         this.partRepository = partRepository;
+        this.orderRepository = orderRepository;
     }
 
     void addContrahent(Contrahent contrahent) throws Exception{
@@ -22,6 +25,9 @@ public class ContrahentService {
 
     public void delete(int id) throws ContrahentRemoveNotAllowedException {
         if (partRepository.existsByContrahentEquals(contrahentRepository.findById(id).get().getFullName())){
+            throw new ContrahentRemoveNotAllowedException("Contrahent in use, con not delete");
+        }
+        if (orderRepository.existsByContrahentEquals(contrahentRepository.findById(id).get().getFullName())){
             throw new ContrahentRemoveNotAllowedException("Contrahent in use, con not delete");
         }
         contrahentRepository.deleteById(id);
--- src/main/java/pl/wojciechsiwek/OpenProfWarehouse/orders/OrderRepository.java
+++ src/main/java/pl/wojciechsiwek/OpenProfWarehouse/orders/OrderRepository.java
@@ -5,4 +5,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+    boolean existsByContrahentEquals(String contrahent);
+
 }
